2f8a9a3bf48c669f8f507bae02a93c7a5038b502
Kat Dearstyne <kdearsty@nd.edu>
2026-01-20 03:01:10 -0600

    bugfix

--- tmp/main.py
+++ tmp/main.py
@@ -4,12 +4,12 @@ Main entry point for running L0 analysis with Gemma Scope 2 CLTs.
 import os
 
 from run_l0 import run_l0_for_all_configs
-
+from src.utils import get_env_list
 
 if __name__ == "__main__":
     results = run_l0_for_all_configs(
         config_dir="l0",
         model_variant="1b-it",
         clt_config="width_262k_l0_medium",
-        config_names=os.environ.get("CONFIG_NAME")
+        config_names=get_env_list("CONFIG_NAME")
     )
